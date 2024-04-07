e7f6c6f46d70682b2494041c4ca28474a86e12a9
Tomi <tomasagustinbrizuela@gmial.com>
2024-04-07 11:13:48 -0300

    Agregué un popup

---
+++ code.js
@@ -0,0 +1,48 @@
+let start = document.querySelector('.start');
+let popup = document.querySelector('.popup');
+let back = document.querySelector('#overlay');
+let close = document.querySelector(".close");
+let mic;
+let selfie;
+let cam;
+
+let options = false;
+
+
+
+
+
+async function media(){
+    const stream = await navigator.mediaDevices.getDisplayMedia({
+        video: true,
+        audio: true
+    })
+
+    const mediarecorder = new MediaRecorder(media, {
+        mimeType: 'video/webm;codecs=vp8,opus'
+      })
+
+    const mediaStream = new MediaRecorder(stream,mimeType);
+}
+
+start.addEventListener('click',function(){
+    if(options === false){
+    popup.style.height = "130px"; 
+    popup.style.visibility = "visible"; 
+    options = true;
+    back.style.display = "block"
+}
+    else{
+    popup.style.height = "0px"; 
+    popup.style.visibility = "hidden"; 
+    options = false;
+    back.style.display = "block"
+    }
+})
+
+close.addEventListener('click',function(){
+    popup.style.height = "0px"; 
+    popup.style.visibility = "hidden"; 
+    options = false;
+    back.style.display = "none"
+})
